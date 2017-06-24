ee6fd338942c43effb9643d45d14d2b223812407
Chintav Shah <chintavmshah@gmail.com>
2017-06-23 22:54:18 -0600

    Added redirection

--- login.php
+++ login.php
@@ -26,9 +26,6 @@ $conn->close();
 ?>
 
 	<form action="login.php" method="post">				<!--Redirect if credentials are wrong-->
-   		Name: <input type="text" name="username"><br>
-   		Password: <input type="password" name="pass"><br>
-   		<input type="submit" value="Login">
 	</form>
 </body>
 </html>
