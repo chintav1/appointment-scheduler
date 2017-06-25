bb8864324905f7ba764c06caef10a5a769177546
Chintav Shah <chintavmshah@gmail.com>
2017-06-25 15:01:50 -0600

    Testong output of SQL query

--- profile.php
+++ profile.php
@@ -2,7 +2,21 @@
 	<body>
 
 		<?php
-			printf('<b> Name: </b>');
+			$servername = "127.0.0.1";
+			$dbusername = "root";
+			$db = "sct";
+
+			$conn = mysqli_connect($servername, $dbusername,"", $db);
+			
+			if ($conn->connect_error) {
+				echo "Connection Error";
+			}
+			
+			$sql = "SELECT * FROM login JOIN patient ON login.Health_Card_No = patient.HEALTH_NO";
+
+			$result = mysqli_query($conn, $sql);
+
+			echo "$result";
 		?>
 
 
