d517a8f91ffd3e9f2b98030273df1b7a34cae246
Chintav Shah <chintavmshah@gmail.com>
2017-06-24 18:39:13 -0600

    Added link to timetable.php

--- login.php
+++ login.php
@@ -19,8 +19,8 @@
 	$num_rows = mysqli_num_rows($result);
 	
 	if($num_rows > 0) {
-    	echo "Welcome, $username";
-    	echo '<a href="http://70.77.112.86/schedule/timetable.php">Click here</a>';
+    	echo "Welcome, $username\r\n";
+    	echo '<a href="http://70.77.112.86/schedules/timetable.php">Click here</a>';
     	die();
 
 	}
