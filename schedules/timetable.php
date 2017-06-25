277da1914363b293e3a7c046a50398fa3c94c6fc
zibinmei <zibinmei@outlook.com>
2017-06-25 11:47:49 -0600

    working table???

--- schedules/timetable.php
+++ schedules/timetable.php
@@ -52,29 +52,42 @@
 						$dbusername = "root";
 						$db = "sct";
 						$opentimeslot = ["10:00","11:00","12:00","13:00","14:00","15:00","16:00","17:00"];
-
+						$todaydate = date('Y-m-d');
+						$dayofweek = date('N');
 						$conn = mysqli_connect($servername, $dbusername,"", $db);
 						if ($conn->connect_error) {
 							echo "Connection Error";
 						}
-// get the name of the doctor that is on schedule this date and time
-
+						// generate the row of the table base on opening timesort
 			            foreach ($opentimeslot as $atime ) {
 
 			            ?>
 			                <tr>
 								<th><?php echo $atime;?></th>
 								<?php
-								$result = mysqli_query("SELECT name FROM employee WHERE (employee)");
+								// get the name of the doctor that is on schedule this date and time
 
+								for ($i =1; $i <6 ;$i++){
+									$j = $i - $dayofweek
+									$thisdate = date($todaydate, strtotime("+({$j}) days"));
+									$result = mysqli_query("SELECT name FROM employee WHERE emp_id IN (SELECT employee_id From schedule WHERE patient_id IS NULL avaliable_time =".$atime."avaliable_date=".$thisdate );
 								?>
 
 
+								<td>
+								<?php while($row = $results->fetch_assoc()){
+								?>
+									<button type="button"><?php echo $row['name']?><button>
+
+								<?php
+								}
+								?>
+								</td>
 
 
-
-
-
+								<?php
+								}
+								?>
 							</tr>
 			            <?php
 			            }
