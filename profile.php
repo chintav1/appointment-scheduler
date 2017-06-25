4488cd6bed0e084b7758014955015aa96a9ee2fd
Chintav Shah <chintavmshah@gmail.com>
2017-06-25 16:48:47 -0600

    Testing output of SQL query

--- profile.php
+++ profile.php
@@ -17,7 +17,7 @@
 
 			$result = mysqli_query($conn, $sql);
 
-			while($row = mysqli_fetch_assoc($conn, $result)) {
+			while($row = mysqli_fetch_assoc($result)) {
    				$name = $row['Name'];
    				//$address = $row['ADDRESS'];
    				//$email = $row['EMAIL'];
