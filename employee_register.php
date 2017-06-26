ec21cafc6835b816aa92217994992eb9eeea1c47
Chintav Shah <chintavmshah@gmail.com>
2017-06-26 15:34:53 -0600

    Added employee registration

---
+++ employee_register.php
@@ -0,0 +1,32 @@
+<html>
+	<body>
+		<form action="patient_registration.php" method="post">
+
+			Name:<br>
+			<input type="text" name="name"><br>
+
+			Address:<br>
+			<input type="text" name="address"><br>
+
+			Email: <br>
+			<input type="email" name="email"><br>
+
+			Certification: <br>
+			<select name = "Certification">
+				<option value = "">Select...</option>
+				<option value = "Dentist">Dentist</option>
+				<option value="Hygenist">Hygenist</option>
+			</select>	
+			------------------------------------------------------------<br>
+			Username: <br>
+			<input type= "text" name="username"><br>
+
+   			Password: <br>
+			<input type="password" name="pass"><br>
+   			<input type="submit" value="Register">
+   		</form>
+
+
+
+	</body>
+</html>
