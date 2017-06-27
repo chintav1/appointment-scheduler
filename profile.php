e7b5f5feeb4cea42ee368cf24bab892d7b5070d2
Chintav Shah <chintavmshah@gmail.com>
2017-06-27 10:17:53 -0600

    Fixing profile

--- profile.php
+++ profile.php
@@ -2,6 +2,7 @@
 	<body>
 
 		<?php
+			$pid = $_GET["pid"];
 			$servername = "127.0.0.1";
 			$dbusername = "root";
 			$db = "sct";
@@ -12,7 +13,7 @@
 				echo "Connection Error";
 			}
 
-			$sql = "SELECT patient.name, patient.address, patient.email, patient.phone FROM login, patient WHERE (login.type_id = patient.id AND login.type = 'patient')";
+			$sql = "SELECT patient.name, patient.address, patient.email, patient.phone FROM login, patient WHERE (login.type_id = patient.id AND login.type = 'patient' AND patient.id = '$pid')";
 
 			$result = mysqli_query($conn, $sql);
 			$num_rows = mysqli_num_rows($result);
