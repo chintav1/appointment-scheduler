e5fa361614fc6fb1c9080e01de454114475a23cb
Chintav Shah <chintavmshah@gmail.com>
2017-06-27 11:18:39 -0600

    Fixing medical record

--- medical_record.php
+++ medical_record.php
@@ -22,7 +22,7 @@
 			$num_patients = mysqli_num_rows($patient_result);
 
 			if ($num_patients > 0) {
-				$update_record = "UPDATE medical_history SET allergies = '$allergy', note = '$note' ";
+				$update_record = "UPDATE medical_history SET allergies = '$allergy', note = '$note' WHERE patient_id = '$PID' ";
 				$result = mysqli_query($conn, $update_record);
 
 				if($result == TRUE) {
