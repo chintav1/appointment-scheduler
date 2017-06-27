f704e36b2550f2bba1d7346af2b9dd6d447e3cdc
zibinmei <zibinmei@outlook.com>
2017-06-27 00:38:49 -0600

    trying

--- schedules/createschedule.php
+++ schedules/createschedule.php
@@ -6,8 +6,6 @@
 
 			$eid = $_POST['eid'];
             $date = $_POST['date'];
-            $stime=$_POST['starttime'];
-            $etime=$_POST['endtime'];
 			$servername = "127.0.0.1";
 			$dbusername = "root";
 			$db = "sct";
@@ -17,11 +15,7 @@
 				echo "Connection Error";
 			}
 
-			$patient_check = "SELECT HEALTH_NO FROM patient WHERE (HEALTH_NO = '$health')";		//checking database for existing patient
 
-			$result = mysqli_query($conn, $patient_check);
-
-			$num_rows = mysqli_num_rows($result);
             $timesopen = ['10','11','12','13','14','15','16','17'];
 
             foreach ($timesopen as $timename) {
