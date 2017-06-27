3f0dcaf743f093658ec9f9c5a75699ac8fb8c44e
zibinmei <zibinmei@outlook.com>
2017-06-27 11:01:05 -0600

    jajaja

--- schedules/cancelschedulefront.php
+++ schedules/cancelschedulefront.php
@@ -12,12 +12,29 @@
             echo "Connection Error";
         }
 
+        $nopatient="SELECT * FROM schedule WHERE employee_id = '$eid' AND patient_id IS NULL";
+        $result = mysqli_query($conn, $nopatient);
+        $num_rows = mysqli_num_rows($result);
+        if($num_rows > 0){
+            echo "Your scheduled time with no patient:"
+            echo '<ul style="list-style-type:none">';
+            while($row = $result->fetch_assoc()){
+               echo '<li>';
+               echo "<a href='cancelschedule.php?eid=".$row['employee_id']."&deldate=".$row['avalible_date']."&deltime=".$row['avalible_time']."'>";
+               echo "CANCEL &#9";
+               echo "</a>";
+               echo $row['avalible_date']." at ".$row['avalible_time'];
+               echo '</li>';
+            }
+           echo '</ul>';
+        }
+
         $sql = "SELECT s.employee_id, s.avalible_date, s.avalible_time,  p.name FROM schedule AS s, patient AS p WHERE s.patient_id=p.id AND s.employee_id='$eid' ORDER BY s.avalible_date";
         $result = mysqli_query($conn, $sql);
         $num_rows = mysqli_num_rows($result);
         if($num_rows > 0){
 
-            echo "Your existing schedules:";
+            echo "Your booked appointment: ";
             echo '<ul style="list-style-type:none">';
             while($row = $result->fetch_assoc()){
                echo '<li>';
