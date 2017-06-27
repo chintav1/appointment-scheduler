0e9547b311857ac09a78733e8753f65d44dfdc42
Chintav Shah <chintavmshah@gmail.com>
2017-06-26 19:00:09 -0600

    Added employee registration

--- employee_registration.php
+++ employee_registration.php
@@ -33,7 +33,7 @@
 				$login = "INSERT INTO login(username, pass, Name) VALUES ('$username', '$password', '$name')";
 				$clinic = "INSERT INTO clinic(NAME, ADDRESS, PHN_NO, HOURS) VALUES ('$clinic_name', '$clinic_address', '$clinic_phone', '$clinic_hours')";
 				
-				if ($conn->query($sql) === TRUE) {
+				if ($conn->query($sql) == TRUE) {
     				echo "New employee record created successfully";
 				}
 
