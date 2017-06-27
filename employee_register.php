12a740a0eefd4d2203f7ffd4805291c4fbe922a8
Chintav Shah <chintavmshah@gmail.com>
2017-06-26 18:02:52 -0600

    Added employee registration

--- employee_register.php
+++ employee_register.php
@@ -2,38 +2,36 @@
 	<body>
 		<form action="employee_registration.php" method="post">
 
-			Name:<br>
+			Name:  (*)<br>
 			<input type="text" name="name"><br>
-
-			Address:<br>
-			<input type="text" name="address"><br>
 
 			Email: <br>
 			<input type="email" name="email"><br>
 
-			Certification: <br>
+			Certification:  (*) <br>
 			<select name = "Certification">
 				<option value = "">Select...</option>
 				<option value = "Dentist">Dentist</option>
-				<option value="Hygenist">Hygenist</option>
+				<option value = "Hygenist">Hygenist</option>
+				<option value = "Secretary">Secretary</option>  
 			</select>	<br>
 
-			Clinic Name: <br>
+			Clinic Name: (*) <br>
 			<input type = "text" name = "c_name"><br>
 
-			Clinic Address: <br>
+			Clinic Address: (*) <br>
 			<input type = "text" name = "c_address"><br>
 
-			Clinic Phone Number: <br>
+			Clinic Phone Number:  (*)<br>
 			<input type = "number" name = "c_number"><br>
 
-			Clinic Hours: <br>
+			Clinic Hours: (*) <br>
 			<input type = "time" name = "c_time"><br>
 			------------------------------------------------------------<br>
-			Username: <br>
+			Username: (*) <br>
 			<input type= "text" name="username"><br>
 
-   			Password: <br>
+   			Password: (*) <br>
 			<input type="password" name="pass"><br>
    			<input type="submit" value="Register">
    		</form>
--- employee_registration.php
+++ employee_registration.php
@@ -1,6 +1,45 @@
 <html>
 	<body>
-		
+		<?php
+		//Getting user info from the form
+			$name = $_POST["name"];
+			$email = $_POST["email"];
+			$certificate = $_POST["Certification"];
+			$clinic_name = $_POST["c_name"];
+			$clinic_address = $_POST["c_address"];
+			$clinic_phone = $_POST["c_number"];
+			$clinic_hours = $_POST["c_time"];
+			$username = $_POST["username"];
+			$password = $_POST["pass"];
+
+		//Server information
+			$username = $_POST["username"];
+			$password = $_POST["pass"];
+			$servername = "127.0.0.1";
+			$dbusername = "root";
+			$db = "sct";
+
+		//Connect to the server
+			$conn = mysqli_connect($servername, $dbusername,"", $db);
+			
+			if ($conn->connect_error) {
+				echo "Connection Error";
+			}
+
+		//Checking for empty required fields
+			if(($name == "") OR ($certificate == "Select...") OR ($clinic_name == "") OR ($clinic_address == "") OR ($clinic_phone == "") OR ($clinic_hours == "") OR ($username == "") OR ($password == "")) {
+				echo "Fields with a (*) are required";
+				die();
+			}
+		//Queries based on type of employee
+
+				$sql = "INSERT INTO employee(NAME, EMAIL, CERTIFICATION) VALUES ('$name', '$email', '$certificate')";
+				
+				if ($conn->query($sql) === TRUE) {
+    				echo "New employee record created successfully";
+				}
+
+		?>
 
 	</body>
 </html>
