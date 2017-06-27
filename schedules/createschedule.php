3c563327dab923217907fb7068f02aa2296f53ec
zibinmei <zibinmei@outlook.com>
2017-06-27 10:35:22 -0600

    jajaja

--- schedules/createschedule.php
+++ schedules/createschedule.php
@@ -18,25 +18,30 @@
 
             $timesopen = ['10','11','12','13','14','15','16','17'];
 
+
+			if
             foreach ($timesopen as $timename) {
 
                 if (isset($_POST[$timename])){
                     $time=$timename.":00:00";
-                    $sql = "INSERT INTO schedule(employee_id,avalible_date,avalible_time) VALUES ('$eid','$date','$time')";
-                    $conn->query($sql);
+					$checking = "SELECT * FROM schedule WHERE employee_id='$eid' AND avalible_date= '$date' AND avalible_time= '$time'";
+					$result =$conn->query($sql);
+ 					if(mysqli_num_rows($result) <= 0)
+  					{
+	                    $sql = "INSERT INTO schedule(employee_id,avalible_date,avalible_time) VALUES ('$eid','$date','$time')";
+	                    $conn->query($sql);
+					}
                 }
             }
 
 			if ($conn->query($sql) == TRUE) {
 				echo "appointment created <br>";
-				echo '<a href="createschedulefront.php?pid='.$eid.'">Add new schedule</a>';
-				echo '&nbsp;&nbsp;&nbsp;&nbsp;<a href="cancelschedulefront.php?pid='.$eid.'">remove existing schedule</a>';
-
 			}
 			else {
 				echo "Error: " . $sql . "<br>" . $conn->error;
 			}
-
+			echo '<a href="createschedulefront.php?pid='.$eid.'">Add new schedule</a>';
+			echo '&nbsp;&nbsp;&nbsp;&nbsp;<a href="cancelschedulefront.php?pid='.$eid.'">remove existing schedule</a>';
 			$conn->close();
 
 		?>
