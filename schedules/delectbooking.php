094751a021a76d2bd2b80213b6ade9e583539a97
zibinmei <zibinmei@outlook.com>
2017-06-26 22:25:17 -0600

    ..

---
+++ schedules/delectbooking.php
@@ -0,0 +1,37 @@
+<html>
+	<body>
+		<?php
+            $eid = $_GET['eid'];
+			$pid = $_GET['pid'];
+            $deldate= $_GET['date'];
+            $deltime= $_GET['time'];
+			$servername = "127.0.0.1";
+			$dbusername = "root";
+			$db = "sct";
+            $date = date('Y-m-d',strtotime("monday this week"));
+			$conn = mysqli_connect($servername, $dbusername,"", $db);
+			if ($conn->connect_error) {
+				echo "Connection Error";
+			}
+
+
+			$sql = "UPDATE schedule SET patient_id= NULL WHERE employee_id='$eid' AND avalible_date='$deldate' AND avalible_time='$deltime'";
+
+
+
+			if ($conn->query($sql) == TRUE) {
+    			echo "appointment canceled <br>";
+                echo '<a href="timetable.php?pid='.$pid.'&date='.$date.'">';
+                echo "go back";
+                echo "</a>";
+			}
+			else {
+    			echo "Error: " . $sql . "<br>" . $conn->error;
+			}
+
+			$conn->close();
+
+		?>
+
+	</body>
+</html>
--- schedules/timetable.php
+++ schedules/timetable.php
@@ -119,9 +119,20 @@
 
 				<?php
 				echo "Your booked appointments: <br>";
-				echo "string";
-
-
+				$sql = "SELECT s.avalible_date, s.avalible_time, e.name, e.id FROM schedule AS s, employee AS e WHERE s.patient_id='$pid' AND s.employee_id=e.id ORDER BY s.avalible_date";
+				$result = $conn->query($sql);
+				echo '<ul style="list-style-type:none">';
+				if($result->num_rows > 0){
+				   while($row = $result->fetch_assoc()){
+					   echo '<li>';
+					   echo "<a href='booking.php?eid=".$row['id']."&deldate=".$row['avalible_date']."&deltime=".$row['avalible_time']."&pid=".$pid."'>";
+					   echo "cancel";
+					   echo "</a>";
+					   echo $row['avalible_date']." at ".$row['avalible_time']." with ".$row['name'];
+					   echo '</li>';
+				   }
+			   	}
+				echo '</ul>';
 
 
 
