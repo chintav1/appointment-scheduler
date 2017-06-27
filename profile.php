b4bef9977ce88420ae75d85388d9711beda3f800
Chintav Shah <chintavmshah@gmail.com>
2017-06-27 10:07:13 -0600

    Fixing profile

--- profile.php
+++ profile.php
@@ -15,14 +15,23 @@
 			$sql = "SELECT name FROM login AND patient WHERE (login.type_id = patient.id AND login.type = 'patient')";
 
 			$result = mysqli_query($conn, $sql);
+			$num_rows = mysqli_num_rows($result);
 
-			while($row == $result -> fetch_assoc()) {
-   				$name = $row['name'];
-   				$address = $row['address'];
-   				$email = $row['email'];
-   				$phone = $row['phone'];
-   				printf("<b> Name: </b> %s", $name);
+			if($num_rows > 0) {
+				while($row = $result -> fetch_assoc()) {
+   					$name = $row['name'];
+   					$address = $row['address'];
+   					$email = $row['email'];
+   					$phone = $row['phone'];
+   					printf("<b> Name: </b> %s", $name);
+				}
 			}
+
+			else{
+				echo "No user found";
+			}
+
+			
 
 		
 
