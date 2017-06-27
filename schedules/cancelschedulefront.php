87f41ec2da450a9a15302397c59c34e38c2cf4f7
zibinmei <zibinmei@outlook.com>
2017-06-27 09:19:30 -0600

    hi

--- schedules/cancelschedulefront.php
+++ schedules/cancelschedulefront.php
@@ -12,22 +12,28 @@
             echo "Connection Error";
         }
 
-        echo "Your existing schedules:";
 
         $sql = "SELECT s.avalible_date, s.avalible_time, s.employee_id, p.name, FROM schedule AS s, patient AS p WHERE s.patient_id=p.id AND s.employee_id='$eid' ORDER BY s.avalible_date";
         $result = $conn->query($sql);
-        echo '<ul style="list-style-type:none">';
+
         if($result->num_rows > 0){
-           while($row = $result->fetch_assoc()){
+
+            echo "Your existing schedules:";
+            echo '<ul style="list-style-type:none">';
+            while($row = $result->fetch_assoc()){
                echo '<li>';
                echo "<a href='cancelschedule.php?eid=".$row['employee_id']."&deldate=".$row['avalible_date']."&deltime=".$row['avalible_time']."'>";
                echo "CANCEL &#9";
                echo "</a>";
                echo $row['avalible_date']." at ".$row['avalible_time']." with ".$row['name'];
                echo '</li>';
-           }
+            }
+           echo '</ul>';
         }
-        echo '</ul>';
+        else{
+            echo "you have no schedule.";
+        }
+
 
         ?>
 
