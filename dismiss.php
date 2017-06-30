002fb634da19c6c3f61a63d60ed6db4fc8074865
zibinmei <zibinmei@outlook.com>
2017-06-30 14:55:32 -0600

    notification

--- dismiss.php
+++ dismiss.php
@@ -16,9 +16,24 @@
 
     mysqli_query($conn, $sql);
 
+    echo "Welcome, $username, your Patient ID is $pid"."<br/>";
+    echo '<a href="schedules/timetable.php?pid='.$pid.'&date='.$date.'">Schedule</a>';
+    echo '&nbsp;&nbsp;&nbsp;&nbsp;<a href="profile.php?pid='.$pid.'">Profile</a>';
+    echo '&nbsp;&nbsp;&nbsp;&nbsp;<a href="record.php?pid='.$pid.'">Medical Record</a>';
+    echo "<br><br><br>";
 
+    //Nodifacation
+    $sql = "SELECT * FROM notification WHERE login_user = '$username'";
+    $result = mysqli_query($conn, $sql);
+    $num_rows = mysqli_num_rows($result);
+    if ($num_rows > 0){
+        echo '<font face="verdana" color="blue">Notification:</font><br>';
+        while ($row =$result -> fetch_assoc()){
+            echo '<a href="dismiss.php?id='.$row['id'].'">DISMISS</a>';
+            echo '&nbsp;'.$row['message'];
+        }
+    }
     die();
-    header('Location: ' . $_SERVER['HTTP_REFERER']);
 
 
 ?>
